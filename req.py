1478078d53451b679fecc5422af757b78ae49d58
NetworkChukka <89191830+NetworkChukka@users.noreply.github.com>
2021-10-17 10:35:59 +0530

    Create req.py

---
+++ req.py
@@ -0,0 +1,3 @@
+print("subscribe My Youtube Channel:=https://www.youtube.com/channel/UCm90WQutvuDC32qCjQ60NCw I am Randika Subash Udayanga 👨‍🦱👨‍🦱👨‍🦱👨‍🦱👨‍🦱👨‍🦱👨‍🦱👨‍🦱👨‍🦱👨‍🦱👨‍🦱The Youtuber In Srilanka My Blog := networkchukka.blogspot.com ගිහිල්ලම බලන්න හොදේ")
+pip install requests
+pip install json
